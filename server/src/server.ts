93f95769a2ce8760bbfe141d60a6f6f453ec8ac6
nazmisani <nazminouval@gmail.com>
2025-05-15 20:56:50 +0700

    feat: enhance Prisma schema with User, Skill, Project, Tag, and ProjectTag models; add seed and router files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import router from "./routers";
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
