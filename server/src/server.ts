6a92e1a3f095d3641c38850e0e828095249270cf
nazmisani <nazminouval@gmail.com>
2025-05-15 20:29:40 +0700

    refactor: update dependencies and remove caching and queries files
    - Added @types/express and express as dependencies in package.json.
    - Removed caching.ts and queries.ts files as they are no longer needed.
    - Created a new server.ts file to set up a basic Express server.

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
