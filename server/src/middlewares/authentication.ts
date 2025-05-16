183a2d7e251340b3405bfec84d8f22f03eb133ff
nazmisani <nazminouval@gmail.com>
2025-05-16 10:12:33 +0700

    feat: implement authentication middleware and extend Express Request type with loginInfo

---
+++ server/src/middlewares/authentication.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express";
+import { verifyToken } from "../helpers/jwt";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authentication = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) throw { name: "Unauthorized" };
+
+    const token = authorization.split(" ")[1];
+
+    const payload = verifyToken(token);
+
+    if (typeof payload === "string") {
+      throw { name: "Unauthorized" };
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: payload.email,
+      },
+    });
+
+    if (!user) throw { name: "Unauthorized" };
+
+    req.loginInfo = {
+      email: user.email,
+      name: user.name,
+    };
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -2,4 +2,6 @@ import { Router } from "express";
 
 const router = Router();
 
+router.post("/auth/register");
+
 export default router;
