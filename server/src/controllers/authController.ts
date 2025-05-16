803467c396cf08ab5ed247c8f648830f233428e4
nazmisani <nazminouval@gmail.com>
2025-05-16 10:40:43 +0700

    feat: implement user registration and login functionality in authController

---
+++ server/src/controllers/authController.ts
@@ -0,0 +1,71 @@
+import { PrismaClient } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+import { comparePassword, hashPassword } from "../helpers/bcrypt";
+import { signToken, verifyToken } from "../helpers/jwt";
+
+const prisma = new PrismaClient();
+
+export default class authController {
+  static async register(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { name, email, password } = req.body;
+
+      if (!name || !email || !password) {
+        res.status(400).json({ message: "All fields are required" });
+      }
+
+      const existingEmail = await prisma.user.findUnique({ where: email });
+
+      if (existingEmail) {
+        return res.status(409).json({ message: "Email already in use" });
+      }
+
+      const hashedPassword = hashPassword(password);
+
+      const user = await prisma.user.create({
+        data: { name, email, password: hashedPassword },
+      });
+
+      res.status(201).json({
+        message: "register success",
+        data: {
+          id: user.id,
+          name: user.name,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+
+  static async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) throw { name: "BadRequest" };
+
+      const user = await prisma.user.findUnique({ where: email });
+
+      if (!user) throw { name: "LoginError" };
+
+      if (!comparePassword(password, user.password))
+        throw { name: "LoginError" };
+
+      const payload = {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      };
+
+      const token = signToken(payload);
+
+      res.status(201).json({
+        token: token,
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+}
