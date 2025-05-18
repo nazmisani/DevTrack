8a3a4d18aa89ec745ab9e7f2734a5771ab7aa0b4
nazmisani <nazminouval@gmail.com>
2025-05-18 23:21:23 +0700

    feat: add authentication middleware to router for enhanced security

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -3,8 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 import AuthController from "../controllers/authController";
+import authentication from "../middlewares/authentication";
 
 router.post("/auth/login", AuthController.login);
 router.post("/auth/register", AuthController.register);
+
+router.use(authentication);
 
 export default router;
