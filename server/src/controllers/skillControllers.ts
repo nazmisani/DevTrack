20476b4d986bfd8902bdb42de185f6fb65e058c7
nazmisani <nazminouval@gmail.com>
2025-05-18 23:47:01 +0700

    fix: correct import path for PrismaClient and improve getSkill method implementation

--- server/src/controllers/skillControllers.ts
+++ server/src/controllers/skillControllers.ts
@@ -1,14 +1,38 @@
-import { PrismaClient } from "@prisma/client/scripts/default-index";
+import { PrismaClient } from "@prisma/client";
+
 import { Request, Response, NextFunction } from "express";
+
 const prisma = new PrismaClient();
 
 class skillController {
+  static async getSkill(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { userId } = req.params;
+
+      const skill = prisma.skill.findMany({
+        where: {
+          userId: Number(userId),
+        },
+      });
+
+      res.status(201).json({
+        data: skill,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   static async addSkill(req: Request, res: Response, next: NextFunction) {
     try {
       const { userId } = req.params;
       const { name, category, status } = req.body;
+
+      res.status(201);
     } catch (error) {
       console.log(error);
     }
   }
 }
+
+export default skillController;
