2ce3bc161972015da3b17fb99e2a1079da87f813
nazmisani <nazminouval@gmail.com>
2025-05-18 23:39:07 +0700

    feat: implement addSkill method in skillController to handle skill creation

--- server/src/controllers/skillControllers.ts
+++ server/src/controllers/skillControllers.ts
@@ -3,5 +3,12 @@ import { Request, Response, NextFunction } from "express";
 const prisma = new PrismaClient();
 
 class skillController {
-  static async addSkill(req: Request, res: Response, next: NextFunction) {}
+  static async addSkill(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { userId } = req.params;
+      const { name, category, status } = req.body;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
