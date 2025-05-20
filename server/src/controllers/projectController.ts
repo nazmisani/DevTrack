41ea506248ac9516d6458bc78acd5b0e0f140c5e
nazmisani <nazminouval@gmail.com>
2025-05-20 22:51:19 +0700

    feat: implement ProjectController with basic CRUD methods and error handling

---
+++ server/src/controllers/projectController.ts
@@ -0,0 +1,49 @@
+import { NextFunction, Request, Response } from "express";
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+class ProjectController {
+  static async getProject(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+  static async getProjectDetail(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+  static async addProject(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+  static async updateProject(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+  static async deleteProject(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+}
+
+export default ProjectController;
